7af1f4677435ab650d74c860e19fd556eef0731f
Vinay Kumar <vinayleokumar@gmail.com>
2021-03-17 19:11:55 +0530

    Added login and logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -12,10 +13,11 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" component={HomeScreen} exact />
+          <Route path="/login" component={LoginScreen} exact />
           <Route path="/product/:id" component={ProductScreen} exact />
           {/*'?' makes the parameter optional */}
-          <Route path="/cart/:id?" component={CartScreen}/>
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
