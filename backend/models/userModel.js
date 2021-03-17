607d1eae91a9ddf80223e8b4e69d50e54ef2c822
Vinay Kumar <vinayleokumar@gmail.com>
2021-03-17 17:59:57 +0530

    completed basic frontend and backend

---
+++ backend/models/userModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  //check for whether password is modified or not. If other data are
+  //are modified not password, then we do not want to re-hash the password as
+  //it will change the hash and user will not be able to login again.
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
---
+++ frontend/src/store.js
@@ -0,0 +1,30 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+});
+
+const cartItemsFormLocalStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFormLocalStorage },
+};
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
